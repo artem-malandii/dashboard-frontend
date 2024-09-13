5e3b622134bad7040662338ac009764df79e1695
Artem Malandii <artem.mal@code-care.pro>
2024-09-13 09:49:23 +0200

    project init

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './app.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      Hello world
+    </div>
+  );
+}
+
+export default App;
